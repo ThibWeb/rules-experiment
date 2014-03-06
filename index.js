ef32b55a3664481d26d7931ddd53d489121a5e04
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-06 06:24:11 +0100

    Implement AND and OR

--- index.js
+++ index.js
@@ -13,19 +13,30 @@ var RULES = (function () {
     return op1 != op2;
   }
 
+  function and(op1, op2) {
+    return op1 && op2;
+  }
+
+  function or(op1, op2) {
+    return op1 || op2;
+  }
+
   return {
     evaluate: evaluate,
 
     EGAL: equal,
-    NONEGAL: notEqual
+    NONEGAL: notEqual,
+    ET: and,
+    OU: or
   };
 })();
 
-(function (EGAL, NONEGAL) {
+(function (EGAL, NONEGAL, ET, OU) {
 
   console.log(RULES.evaluate( [ 5 ,EGAL, 5 ] ));
   console.log(RULES.evaluate( [ 5 ,NONEGAL, 3 ] ));
+  console.log(RULES.evaluate( [ false ,OU, false ] ));
 
-})(RULES.EGAL, RULES.NONEGAL);
+})(RULES.EGAL, RULES.NONEGAL, RULES.ET, RULES.OU);
 
 

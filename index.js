1a834b57203d579bf1c4e96f5c75a8641aa70ecb
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-06 06:31:52 +0100

    Implement nested expressions

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ var RULES = (function () {
   'use strict';
 
   function evaluate(expr) {
-    return expr[1](expr[0], expr[2]);
+    if (isRuleExpression(expr)) {
+      return expr[1](evaluate(expr[0]), evaluate(expr[2]));
+    }
+    else {
+      return expr;
+    }
+  }
+
+  function isRuleExpression(expr) {
+    return expr instanceof Array && isFunction(expr[1]);
+  }
+
+  function isFunction(fn) {
+    var obj = {};
+    return fn && obj.toString.call(fn) === '[object Function]';
   }
 
   function equal(op1, op2) {
@@ -35,7 +49,8 @@ var RULES = (function () {
 
   console.log(RULES.evaluate( [ 5 ,EGAL, 5 ] ));
   console.log(RULES.evaluate( [ 5 ,NONEGAL, 3 ] ));
-  console.log(RULES.evaluate( [ false ,OU, false ] ));
+  console.log(RULES.evaluate( [[ 5 ,EGAL, 5 ] ,ET, [ 5 ,NONEGAL, 3 ]] ));
+  console.log(RULES.evaluate( [[ 5 ,EGAL, 5 ] ,OU, [ 5 ,EGAL, 2 ]] ));
 
 })(RULES.EGAL, RULES.NONEGAL, RULES.ET, RULES.OU);
 
